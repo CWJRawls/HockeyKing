dad4014571ce50dfd4cd76706700f0a1b3dd5324
Connor Rawls <cwrawls@asu.edu>
2015-11-14 00:14:14 -0700

    Added Aura Effect Base Class

---
+++ src/com/rawls/data/AuraEffect.java
@@ -0,0 +1,19 @@
+package com.rawls.data;
+
+public class AuraEffect {
+
+	
+	/**
+	 * Class for determining and containing data about the effect certain players have on their teammates
+	 */
+	
+	private int skater_effect, goalie_effect;
+	
+	public AuraEffect(int se, int ge)
+	{
+		skater_effect = se;
+		goalie_effect = ge;
+	}
+	
+	
+}
--- src/com/rawls/data/Skater.java
+++ src/com/rawls/data/Skater.java
@@ -15,12 +15,24 @@ public class Skater extends Player{
 	 * UTILITY
 	 * @param Speed - How far the player will move in a turn
 	 * @param Dexterity - Ability of the skater to corral loose pucks
+	 * @param Balance - How well the skater can stay on their feet.
+	 * @param Injury_Resistance - How likely the skater is to be injured
+	 * @param Hand_eye - Ability of the player to knock pucks down or tip shots
+	 * @param Stamina - How long the skater can move at full speed
 	 * 
 	 * OFFENSIVE
 	 * @param Posession - Difficulty for player to be stick checked
 	 * @param Passing - Likelihood Pass will be on target
+	 * @param Wrist_Shot_Power - How fast the puck is moving when shot
+	 * @param Wrist_Shot_accuracy - How accurate the skater is on the wrist shot
+	 * @param Slap_shot_power - How fast the puck is moving when shot
+	 * @param Slap_shot_accuracy How accurate the skater is on the slap shot
 	 * 
 	 * DEFENSIVE
+	 * @param Body_Checking - Chance body check will be successful
+	 * @param Stick_Checking - Chance Stick Check will be successful
+	 * @param Shot_Blocking - Chance a shot block will be successful
+	 * @param Toughness - How many hits the skater will take in a game before shying away from contact
 	 */
 	
 	public Skater(String f, String l, String p) {
